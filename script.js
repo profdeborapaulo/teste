495d4bb974668a663c6ffa73865906c1375a990d
Debora Batista Paulo <87874470+profdeborapaulo@users.noreply.github.com>
2026-04-08 12:19:47 -0300

    envio arquivos

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcularMedia() {
+    const nota1= parseFloat(document.getElementById('nota1').value);
+    const nota2= parseFloat(document.getElementById('nota2').value);
+    
+    const media= (nota1+nota2)/2;
+
+    const resultado= document.getElementById('resultado');
+    resultado.innerHTML= "Média: " + media.toFixed(2);
+}
